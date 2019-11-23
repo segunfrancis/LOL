84d75d98cbf75324726f6503ca1775075ade3c6b
segunfrancis <francis4dblues@gmail.com>
2019-11-23 19:32:50 +0100

    LOL - Implementation: Google AdMob

--- app/src/main/java/com/project/segunfrancis/lol/MainActivity.kt
+++ app/src/main/java/com/project/segunfrancis/lol/MainActivity.kt
@@ -12,22 +12,33 @@ import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.project.segunfrancis.lol.utils.Utility.Companion.APP_THEME
 import com.project.segunfrancis.lol.utils.Utility.Companion.SHARED_PREF_KEY
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize Google adMob
+        MobileAds.initialize(this) {}
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val pref = getSharedPreferences(SHARED_PREF_KEY, Activity.MODE_PRIVATE)
+        /*val pref = getSharedPreferences(SHARED_PREF_KEY, Activity.MODE_PRIVATE)
         val theme = pref.getInt(APP_THEME, 0)
-        AppCompatDelegate.setDefaultNightMode(theme)
+        AppCompatDelegate.setDefaultNightMode(theme)*/
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
